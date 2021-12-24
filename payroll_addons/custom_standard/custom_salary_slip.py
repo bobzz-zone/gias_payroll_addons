2bf618f51583ae08adf0a75f8a19c330d68fc475
bobzz-zone <bobzz.zone@gmail.com>
2021-12-24 22:58:35 +0700

    Update custom_salary_slip.py

--- payroll_addons/custom_standard/custom_salary_slip.py
+++ payroll_addons/custom_standard/custom_salary_slip.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from dateutil.relativedelta import relativedelta
-from frappe.utils import cint, flt, nowdate, add_days, getdate, fmt_money, add_to_date, DATE_FORMAT, date_diff
+from frappe.utils import cint,cstr, flt, nowdate, add_days, getdate, fmt_money, add_to_date, DATE_FORMAT, date_diff
 from frappe import _
 from erpnext.accounts.utils import get_fiscal_year
 from erpnext.hr.doctype.employee.employee import get_holiday_list_for_employee

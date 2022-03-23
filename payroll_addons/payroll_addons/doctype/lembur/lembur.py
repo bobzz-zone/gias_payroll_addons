fb7e3a872e198aea93d582395f43303b88fec58b
bobzz-zone <bobzz.zone@gmail.com>
2022-03-23 16:24:50 +0700

    lembur

--- payroll_addons/payroll_addons/doctype/lembur/lembur.py
+++ payroll_addons/payroll_addons/doctype/lembur/lembur.py
@@ -2,9 +2,60 @@
 # For license information, please see license.txt
 
 import frappe
+import math
 from frappe.model.document import Document
 
 class Lembur(Document):
 	@frappe.whitelist()
 	def get_data(self):
-		frappe.msgprint("Called")
+		if not self.from_date:
+			frappe.throw("Siliahkan isi Periode Terlebih Dahulu")
+		if not self.to_date:
+			frappe.throw("Siliahkan isi Periode Terlebih Dahulu")
+		if not self.branch:
+			frappe.throw("Siliahkan isi data cabang Terlebih Dahulu")
+		department=""
+		if self.department:
+			department=""" and department="{}" """.format(self.department)
+		#get data pengajuan lembur yang ter approve
+		data_lembur=frappe.db.sql("""select employee,employee_name,
+			sum(IF(is_holiday=0,IF(total<2,total,2),0)) as biasa_jam_awal,sum(IF(is_holiday=0,IF(total<2,total_menit,0),0)) as biasa_menit_awal,
+			sum(IF(is_holiday=0,IF(total<2,0,total-2),0)) as biasa_jam_akhir,sum(IF(is_holiday=0,IF(total<2,0,total_menit),0)) as biasa_menit_akhir,
+			sum(IF(is_holiday=1,IF(total<2,total,2),0)) as libur_jam_awal,sum(IF(is_holiday=1,IF(total<2,total_menit,0),0)) as libur_menit_awal,
+			sum(IF(is_holiday=1,IF(total<2,0,total-2),0)) as libur_jam_akhir,sum(IF(is_holiday=1,IF(total<2,0,total_menit),0)) as libur_menit_akhir
+		 from `tabPengajuan Lembur` 
+			where docstatus=1 and (lembur is NULL or lembur ="") and (tanggal_lembur >= "{}" and tanggal_lembur<="{}") and branch="{}" {} 
+			group by employee""".format(self.from_date,self.to_date,self.branch,department),as_list=1)
+		#clear data before set
+		self.data_lembur_karyawan=[]
+		grand_total=0
+		data_nilai = frappe.get_single("Setting Biaya Lembur")
+		for row in data_lembur:
+			row_data={}
+			#normalize value menit
+			row_data["employee"]=row[0]
+			row_data["employee_name"]=row[1]
+			row_data["jam_hari_biasa_awal"]=row[2]+math.floor(row[3]/60)
+			row_data["menit_hari_biasa_awal"]=row[3]%60
+			row_data["jam_hari_biasa_akhir"]=row[4]+math.floor(row[5]/60)
+			row_data["menit_hari_biasa_akhir"]=row[5]%60
+			row_data["jam_hari_libur_awal"]=row[6]+math.floor(row[7]/60)
+			row_data["menit_hari_libur_awal"]=row[7]%60
+			row_data["jam_hari_libur_akhir"]=row[8]+math.floor(row[9]/60)
+			row_data["menit_hari_libur_akhir"]=row[9]%60
+
+			total_value=0
+			total_value+=data_nilai.biasa_jam_awal*row_data["jam_hari_biasa_awal"]
+			total_value+=data_nilai.biasa_menit_awal*row_data["menit_hari_biasa_awal"]
+			total_value+=data_nilai.biasa_jam_akhir*row_data["jam_hari_biasa_akhir"]
+			total_value+=data_nilai.biasa_menit_akhir*row_data["menit_hari_biasa_akhir"]
+			total_value+=data_nilai.libur_jam_awal*row_data["jam_hari_libur_awal"]
+			total_value+=data_nilai.libur_menit_awal*row_data["menit_hari_libur_awal"]
+			total_value+=data_nilai.libur_jam_akhir*row_data["jam_hari_libur_akhir"]
+			total_value+=data_nilai.libur_menit_akhir*row_data["menit_hari_libur_akhir"]
+
+			row_data["total_value"]=total_value
+			grand_total+=total_value
+			self.append("data_lembur_karyawan",row_data)
+		self.total_cost=grand_total
+
--- payroll_addons/payroll_addons/doctype/pengajuan_lembur/pengajuan_lembur.py
+++ payroll_addons/payroll_addons/doctype/pengajuan_lembur/pengajuan_lembur.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2022, das and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from erpnext.hr.doctype.holiday_list.holiday_list import is_holiday
 from frappe.model.document import Document
 
 class PengajuanLembur(Document):
-	pass
+	def validate(self):
+		emp = frappe.get_doc("Employee",self.employee)
+		if is_holiday(emp.holiday_list,self.tanggal_lembur):
+			self.is_holiday=1
+		else:
+			self.is_holiday=0
+		if self.total_menit > 59 or self.est_menit>59:
+			frappe.throw("Total Menit Tidak boleh lebih dari 1 jam")
+		if self.est_menit and not self.total_menit:
+			self.total_menit=self.est_menit
+		if self.est_jam and not self.total:
+			self.total=self.est_jam
+		

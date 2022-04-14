7b9a8f269affa6052e14efb99673798d31c9d50b
Bobby <bobzz.zone@gmail.com>
2022-04-14 12:12:38 +0000

    aa

--- payroll_addons/payroll_addons/doctype/pengajuan_lembur/pengajuan_lembur.py
+++ payroll_addons/payroll_addons/doctype/pengajuan_lembur/pengajuan_lembur.py
@@ -14,8 +14,23 @@ class PengajuanLembur(Document):
 			self.is_holiday=0
 		if (self.total_menit or 0) > 59 or (self.est_menit or 0) > 59:
 			frappe.throw("Total Menit Tidak boleh lebih dari 1 jam")
-		if self.est_menit and not self.total_menit:
-			self.total_menit=self.est_menit
-		if self.est_jam and not self.total:
-			self.total=self.est_jam
-		
+		#if self.est_menit and not self.total_menit:
+		#	self.total_menit=self.est_menit
+		#if self.est_jam and not self.total:
+		#	self.total=self.est_jam
+		out_time=frappe.db.sql("""select out_time,out_time_detail from `tabAttendance` where employee="{}" and attendance_date="{}" and docstatus=1  """,as_list=1)
+		if out_time and out_time[0]:
+			self.checkout=out_time[0][0]
+			co=out_time[0][1].split(":")
+			jo=int(co[0]) - 18
+			mo=int(co[1])
+			if jo < 0:
+				frappe.throw("Pada tanggal ini tidak chekcout lebih dari seharusnya")
+			elif jo < self.total:
+				frappe.msgprint("Pengajuan Lembur tidak boleh lebih dari jam checkout , nilai terupdate sesuai jam checkout sebagai maximal")
+				self.total=jo
+				self.total_menit=mo
+			elif jo == self.total and mo < self.total_menit:
+				frappe.msgprint("Pengajuan Lembur tidak boleh lebih dari jam checkout , nilai terupdate sesuai jam checkout sebagai maximal")
+				self.total_menit=mo
+				self.total=jo
